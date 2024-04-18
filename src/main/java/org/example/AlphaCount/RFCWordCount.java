7fc54ef8d29cc4f89eff96a415a1f46c785962ff
Surbhi Chaudhary <surbhi.chaudhary@creditsaison-in.com>
2024-04-18 15:56:44 +0530

    SSL blocking resolved

--- src/main/java/org/example/AlphaCount/RFCWordCount.java
+++ src/main/java/org/example/AlphaCount/RFCWordCount.java
@@ -2,39 +2,76 @@ package org.example.AlphaCount;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class RFCWordCount {
 
     private static String fetchFromUrl(String url) throws IOException, InterruptedException {
+        // Create an SSLContext with the trust manager
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                    }
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            final HttpGet httpGet = new HttpGet(url);
-            System.out.println("Executing request " + httpGet.getRequestLine());
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                    }
 
-            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
-                StatusLine statusLine = response.getStatusLine();
-                System.out.println(statusLine.getStatusCode() + " " + statusLine.getReasonPhrase());
-                String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-                System.out.println("Response body: " + responseBody);
-                return responseBody;
-            }
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                }
+        };
+
+        SSLContext sslContext;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
         }
+
+        // Create an HttpClient that uses the custom SSLContext and follows redirects
+        HttpClient client = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        // Use the HttpClient instance you've created
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // Process the response
+        System.out.println("Response code: " + response.statusCode());
+        String responseBody = response.body();
+        System.out.println("Response body: " + responseBody);
+        return responseBody;
     }
 
     public static void main(String args[]) throws IOException, InterruptedException {
         AtomicReference<Integer> count = new AtomicReference<>(0);
 
-        for (int i = 1 ; i <= 1; i++) {
+        for (int i = 1 ; i <= 100; i++) {
             int finalI = i;
             String url = "https://www.rfc-editor.org/rfc/rfc" + finalI + ".txt";
             Thread thread = Thread.ofVirtual().start(() -> {
@@ -45,7 +82,7 @@ public class RFCWordCount {
                     for (char c : rfcContent.toCharArray()) {
                         if (Character.isLowerCase(c)) {
                             count.getAndSet(count.get() + 1);
-                            System.out.println(count);
+                            System.out.println("Current count of words is : "+ count);
                         }
                     }
                     System.out.println("Completed fetching for current url");
@@ -61,107 +98,3 @@ public class RFCWordCount {
         System.out.println("The count of all characters is: " + count);
     }
 }
-/*
-httpClient = HttpClients.custom()
-            .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLContexts.custom()
-                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-                    .build()
-                )
-            ).build();
-
-////////////////////////////////////////////////////////////////////////////////////
-
- private static String fetchFromUrl(String url) throws IOException, InterruptedException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        //making get call to given url
-        HttpGet httpget = new HttpGet(url);
-        System.out.println("Executing request " + httpget.getRequestLine());
-
-        ResponseHandler<String> responseHandler = response -> {
-            int status = response.getStatusLine().getStatusCode();
-            if (status >= 200 && status < 300) {
-                HttpEntity entity = response.getEntity();
-                return entity != null ? EntityUtils.toString(entity) : null;
-            } else {
-                throw new ClientProtocolException("Unexpected response status: " + status);
-            }
-        };
-
-        String responseBody = httpclient.execute(httpget, responseHandler);
-        System.out.println("----------------------------------------");
-        System.out.println(responseBody);
-        return responseBody;
-    }
-
-////////////////////////////////////////////////////////////////////////////////////
-
-HttpResponse httpresponse = httpclient.execute(httpget);
-        Scanner sc = new Scanner(httpresponse.getEntity().getContent());
-
-        //Printing the status line
-        System.out.println(httpresponse.getStatusLine());
-        while(sc.hasNext()) {
-            System.out.println(sc.nextLine());
-        }
-
-////////////////////////////////////////////////////////////////////////////////////
-
-    private static String fetchFromUrl(String url) throws IOException, InterruptedException {
-        System.out.println("In fetch function");
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println( "After fetching from httpClient " + response.body());
-        return response.body();
-    }
-
-////////////////////////////////////////////////////////////////////////////////////
-
-    private static String getContentFromUrl(String urlString) throws IOException {
-        StringBuilder content = new StringBuilder();
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-        } finally {
-            connection.disconnect();
-        }
-
-        return content.toString();
-    }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
- private static String fetchFromUrl(String url) throws IOException, InterruptedException {
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
-        String res = "";
-        System.out.println("In fetch function");
-
-        try {
-            System.out.println("Try block 1 ");
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            System.out.println("Try block 2 ");
-            if (response.statusCode() == 200) {
-                String responseBody = response.body().substring(0, 1000);
-                System.out.println(responseBody);
-                res = responseBody;
-            } else {
-                System.err.println("Request failed with status code: " + response.statusCode());
-            }
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Catch block");
-            e.printStackTrace();
-        }
-        return res;
-    }
-
- */
